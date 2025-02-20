0c4a1bba3b8dcb7b765b0591cd0f5e34742a771a
HungNguyen <www.thuphan@gmail.com>
2025-02-20 19:29:59 +0700

    Add snowy testing stuff!!

---
+++ playground/snowy/main.js
@@ -0,0 +1,14 @@
+let doc = document.getElementById("snowContainer");
+function createSnow() {
+  let elm = document.createElement("div");
+  elm.className = "snow-flake";
+  elm.style.marginLeft = Math.random() * 100 + "%";
+  doc.append(elm);
+  setTimeout(() => {
+    elm.remove();
+  }, 10000);
+}
+
+let a = setInterval(() => {
+  createSnow();
+}, Math.random() * 500);
