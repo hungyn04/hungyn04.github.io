8427f670fbdc562ebe8b24cbf43fbddd335271d4
HungNguyen <www.thuphan@gmail.com>
2025-02-21 23:27:41 +0700

    Massive changes under the hood

--- homepage/js/main.js
+++ homepage/js/main.js
@@ -2,23 +2,22 @@ const documentElm = document.documentElement;
 let scrolly;
 
 function pushPropToDoc(prop, val) {
-    documentElm.style.setProperty(prop, val)
+  documentElm.style.setProperty(prop, val);
 }
 function scrollPrg() {
-    scrolly = window.scrollY;
-    pushPropToDoc('--scroll-progress', scrolly);
-    pushPropToDoc('--scroll-progress-px', scrolly + 'px');
+  scrolly = window.scrollY;
+  pushPropToDoc("--scroll-progress", scrolly);
+  pushPropToDoc("--scroll-progress-px", scrolly + "px");
 }
-window.onscroll = (scrollPrg);
-window.onresize = (scrollPrg);
+window.onscroll = scrollPrg;
 
 function fadeOut() {
-    var doc = document.querySelector("html").style;
-    doc.animation = "slide-out 0.3s cubic-bezier(0.94, 0, 0.99, 0.01), fade-out 0.3s 0.05s both";
+  var doc = document.querySelector("html").style;
+  doc.animation = "slide-out 0.3s cubic-bezier(0.94, 0, 0.99, 0.01), fade-out 0.3s 0.05s";
 }
 
 function copy() {
-    navigator.clipboard.writeText("<empty clipboard>")
+  navigator.clipboard.writeText("<empty clipboard>");
 }
 
 window.onbeforeunload = fadeOut;
