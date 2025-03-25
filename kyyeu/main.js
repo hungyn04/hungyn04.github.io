4bf5169d25ff925783098b60914ba9a8d156ec75
HungNguyen <www.thuphan@gmail.com>
2025-03-25 21:06:42 +0700

    fix bugs

--- kyyeu/main.js
+++ kyyeu/main.js
@@ -1,15 +1,18 @@
-const EVENT = ["touchstart", "touchend", "mousedown", "mouseup", "mouseover", "scroll"];
+const EVENT = ["touchstart", "touchend", "mousedown", "mouseup", "mouseover", "scroll", "wheel", "touchmove"];
 let hoveredBttn = (lastBttn = null);
 let pressedList = [];
 let topBttn = document.getElementById("top");
 let lstScrollPos = 0;
+let anm = null;
 
 checkScrollTopBttn();
 
 EVENT.forEach((act) => {
-  document.addEventListener(act, (event) => {
+  window.addEventListener(act, (event) => {
     switch (act) {
       case "touchstart":
+      case "touchmove":
+        stopAnm();
       case "mousedown":
         let pressedBttn = event.target.closest("button");
         if (pressedBttn) {
@@ -31,22 +34,16 @@ EVENT.forEach((act) => {
           initS = window.scrollY;
           anm = requestAnimationFrame(function _(t) {
             if (intitT == 0) intitT = t;
-            dtTs = (t - intitT) / (750 + initS / 60);
+            dtTs = (t - intitT) / (1000 + initS / 10);
             if (window.scrollY > 0) {
-              window.scroll(0, initS * Math.pow(2, -10 * dtTs));
+              window.scroll(0, initS * (1 - (dtTs < 0.5 ? Math.pow(2, 20 * dtTs - 10) / 2 : (2 - Math.pow(2, -20 * dtTs + 10)) / 2)));
               anm = requestAnimationFrame(_);
-              onwheel = () => {
-                cancelAnimationFrame(anm);
-              };
-              ontouchstart = () => {
-                cancelAnimationFrame(anm);
-              };
-              ontouchmove = () => {
-                cancelAnimationFrame(anm);
-              };
+            } else {
+              anm = null;
             }
           });
         }
+        break;
       case "mouseover":
         hoveredBttn = event.target.closest("button");
         if (hoveredBttn) {
@@ -56,23 +53,36 @@ EVENT.forEach((act) => {
           lastBttn.classList.remove("hover");
           lastBttn = null;
         }
+        break;
       case "scroll":
-        checkScrollTopBttn();
+        checkScrollTopBttn(!anm);
+        break;
+      case "wheel":
+        stopAnm();
     }
   });
 });
 
-function checkScrollTopBttn() {
+function stopAnm() {
+  cancelAnimationFrame(anm);
+  anm = null;
+}
+
+function checkScrollTopBttn(controlExtend = true) {
   if (window.scrollY < 100) {
-    topBttn.style.visibility = "hidden";
+    topBttn.style.opacity = "0";
+    topBttn.style.pointerEvents = "none";
   } else {
-    topBttn.style.visibility = "visible";
+    topBttn.style.opacity = "1";
+    topBttn.style.pointerEvents = "all";
   }
-  if (lstScrollPos - window.scrollY > 5) {
-    topBttn.classList.add("extend");
-  }
-  if (window.scrollY - lstScrollPos > 5) {
-    topBttn.classList.remove("extend");
+  if (controlExtend) {
+    if (lstScrollPos - window.scrollY > 5) {
+      topBttn.classList.add("extend");
+    }
+    if (window.scrollY - lstScrollPos > 5) {
+      topBttn.classList.remove("extend");
+    }
   }
   lstScrollPos = window.scrollY;
 }
